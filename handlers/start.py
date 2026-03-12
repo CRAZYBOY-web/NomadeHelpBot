4c0da22623dedd5968f4b67e0ca4ab042c347511
CRAZYBOY-web <sayandas3409@gmail.com>
2026-03-12 14:10:53 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,13 +1,3 @@
-# ============================================================
-#Group Manager Bot
-# Author: LearningBotsOfficial (https://github.com/LearningBotsOfficial) 
-# Support: https://t.me/LearningBotsCommunity
-# Channel: https://t.me/learning_bots
-# YouTube: https://youtube.com/@learning_bots
-# License: Open-source (keep credits, no resale)
-# ============================================================
-
-
 from pyrogram import Client, filters
 from pyrogram.types import (
     InlineKeyboardButton,
@@ -27,7 +17,7 @@ def register_handlers(app: Client):
 
    ✨ Hello {user}! ✨
 
-👋 I am Nomad 🤖 
+👋 I am ༒ ᴘɪᴋᴀᴄʜᴜᴜ ༒ 🤖 
 
 Highlights:
 ─────────────────────────────
