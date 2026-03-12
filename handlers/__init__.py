46adfeee141f713aeee50c92308aa3984cc04ae0
CRAZYBOY-web <sayandas3409@gmail.com>
2026-03-12 21:00:40 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,10 +4,15 @@
 
 from .start import register_handlers
 from .group_commands import register_group_commands
-from .welcome import register_welcome_handlers  # <--- Added this
+from .welcome import register_welcome_handlers
+from .moderation import register_moderation_handlers
+from .approval import register_approval_handlers
 
 def register_all_handlers(app):
     register_handlers(app)
     register_group_commands(app)
-    register_welcome_handlers(app) # <--- Added this
-    print("✅ All handlers (Start, Group, Welcome) registered!")
+    register_welcome_handlers(app)
+    register_moderation_handlers(app)
+    register_approval_handlers(app)
+    
+    print("✅ ༒ ᴘɪᴋᴀᴄʜᴜᴜ ༒ Systems are all Online!")
