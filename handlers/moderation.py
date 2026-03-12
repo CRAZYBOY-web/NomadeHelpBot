9580c03cd45f5848863bbae0f50075acaf5998fc
CRAZYBOY-web <sayandas3409@gmail.com>
2026-03-12 20:58:35 +0530

    Create moderation.py

---
+++ handlers/moderation.py
@@ -0,0 +1,22 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from db import get_antilink, set_antilink
+
+@Client.on_message(filters.group & ~filters.service, group=-1)
+async def antilink_handler(client, message: Message):
+    if not message.text or not await get_antilink(message.chat.id):
+        return
+    if "t.me/" in message.text or "http" in message.text:
+        user = await client.get_chat_member(message.chat.id, message.from_user.id)
+        if user.status not in ["administrator", "creator"]:
+            await message.delete()
+
+@Client.on_message(filters.command("ban") & filters.group)
+async def ban_user(client, message: Message):
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status in ["administrator", "creator"] and message.reply_to_message:
+        await client.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+        await message.reply_text("✅ Banned!")
+
+def register_moderation_handlers(app):
+    pass # Decorators handle this
